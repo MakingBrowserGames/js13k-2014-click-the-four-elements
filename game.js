29ff8014313ada48532db1c6ce6b2744e4c214f3
robomatix <robomatix@robomatix.net>
2014-09-06 22:22:45 +0200

    Comparison of the element clicked and the element that should be clicked ! Watch the console !

--- game.js
+++ game.js
@@ -2,20 +2,78 @@
  * Created by robomatix on 06/09/14.
  */
 
-var a = ["earth", "water", "air", "fire"];
-shuffle(a);
 
+/*
+* The initial array of elements
+***********************************
+ */
+var a = ["earth", "water", "air", "fire"];
+
+/*
+* Generating the arrays that will be used for the order in which the element should be clicked and the order they will be displayed
+* *********************************************************************************************************************************
+ */
+
+shuffle(a); // Randomize the array of elements
+
+var dB = new Object(); // dB stand for display board. It will be the reference, the order in which the elements should be clicked...
+dB.el1 = a[0];
+dB.el2 = a[1];
+dB.el3 = a[2];
+dB.el4 = a[3];
+
+shuffle(a); // Randomize the array of elements
+
+var dG = new Object(); // dG stand for display game. It will be the order in which the elements will be displayed...
+dG.el1 = a[0];
+dG.el2 = a[1];
+dG.el3 = a[2];
+dG.el4 = a[3];
+
+/* https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Object/getOwnPropertyNames */
+sA1 = new Array();
+Object.getOwnPropertyNames(dB).forEach(function (val, idx, array) {
+
+    console.log(idx + " ->" + val + " -> " + dB[val]);
+    sA1[idx] = dB[val];
+
+});
+
+sA2 = new Array();
+Object.getOwnPropertyNames(dG).forEach(function (val, idx, array) {
+
+    console.log(idx + " ->" + val + " -> " + dG[val]);
+    sA2[idx] = dG[val];
+
+});
+
+console.log(sA1);
+console.log(sA2);
+
+/*
+* Generating the div in the document displayed in the browser
+***************************************************************
+ */
 i = 0;
 for (tot = a.length; i < tot; i++)
     addEl(a[i]);
 
-/* To detected witch element is clicked, not the more more concise way, but it always work */
+/*
+* The logic of the game ! LOL !
+***********************************************************************************************************
+ */
+
+cC = 0; // cC stand for click counter, sort of index of the "clicks", useful to know if the clicked element is the one that should have been clicked. In the array of elements c should be index of the element in the array
+
+/* To detected witch element is clicked, not the more concise way, but it always work */
 elClkd("earth");
 elClkd("water");
 elClkd("air");
 elClkd("fire");
 
-
+/*
+* function that randomize an array
+ */
 /*http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array*/
 function shuffle(array) {
 
@@ -38,11 +96,13 @@ function shuffle(array) {
 
 }
 
-
-
+/*
+ * Handle the creation of the div that represents the elements (e = name of the element )
+ *******************************************************************************************
+ */
 function addEl(e) {
-    /* http://stackoverflow.com/questions/14004117/javascript-create-div-and-append-div-dynamically */
 
+    /* http://stackoverflow.com/questions/14004117/javascript-create-div-and-append-div-dynamically */
     // Create a div
     var iDiv = document.createElement('div');
     iDiv.id = e;
@@ -57,10 +117,25 @@ function addEl(e) {
 
 }
 
-function elClkd(el){
-    document.getElementById(el).onclick = function() {
-        console.log(el);
+/*
+* Handle the logic needed when an element  is clicked (e = name of the element, the id of the div )
+*********************************************************************************************************
+ */
+function elClkd(e) {
+
+    document.getElementById(e).onclick = function () {
+        console.log(sA1);
+        console.log('sa1 : ' + sA1[cC]);
+        console.log(e + " " + cC);
+
+        if (e === sA1[cC]) {// Comparing the element clicked and the one that should have been clicked
+            console.log('Ok');
+        } else {
+            console.log('Ko');
+        }
+        cC++;
     }
+
 }
 
 
